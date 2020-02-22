b58b7c24e90354860e3b710da117ced378877ff2
Nelson HO <nelson_510@yahoo.com.hk>
2020-02-22 13:13:45 +0800

    Add files via upload

---
+++ 278. First Bad Version modify.cpp
@@ -0,0 +1,29 @@
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        //cout<<"N is "<<n<<endl;
+       return findfirstBadVersion(0,n);
+    }
+    
+   int findfirstBadVersion(int l,int r){
+       //cout<<l<<" "<<r<<endl;
+       //cout<<(l==r);
+        if(r==l){
+           return r;
+       }
+        if(r>l){
+        int mid= l + (r - l) / 2;
+        if(!isBadVersion(mid)){         //find it from right
+           return findfirstBadVersion(mid+1,r);            
+        }   
+        else if(isBadVersion(mid)){    //find it from left
+           return findfirstBadVersion(l,mid);
+        }    
+        }
+       
+       return -1;
+    }
+};
